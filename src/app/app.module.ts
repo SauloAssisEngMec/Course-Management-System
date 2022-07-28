75727e73c6928aa06c40a5bbb5a8a53d673b48c4
Saulo <sauloassis4123@gmail.com>
2022-07-28 14:37:12 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './core/component/nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { Error404Component } from './error-404/error.component';
+import { CourseModule } from './courses/course.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    Error404Component,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CourseModule,
+    HttpClientModule,
+    RouterModule.forRoot([ 
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: '**', component: Error404Component
+      }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
